1901d825423e2773d411584bcd1c79cef5aed777
Andro <androistuk@gmail.com>
2023-02-22 20:35:20 +0100

    .

---
+++ src/components/FeedbackList.jsx
@@ -0,0 +1,26 @@
+import PropTypes from "prop-types";
+import FeedbackItem from "./FeedbackItem";
+function FeedbackList({ feedback }) {
+  if (!feedback || feedback.lenght === 0) {
+    return <p>No Feedback YET :)</p>;
+  }
+  return (
+    <div className="feedback-list">
+      {feedback.map((item) => (
+        <FeedbackItem key={item.id} item={item} />
+      ))}
+    </div>
+  );
+}
+
+FeedbackList.propTypes = {
+  feedback: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      rating: PropTypes.number.isRequired,
+      text: PropTypes.string.isRequired,
+    })
+  ),
+};
+
+export default FeedbackList;
---
+++ src/data/Feedbackdata.js
@@ -0,0 +1,24 @@
+const FeedbackData = [
+  {
+    id: 1,
+    rating: 10,
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, sequi.",
+  },
+  {
+    id: 2,
+    rating: 9.9,
+    text: "Ovo je neki drugi feedback komentar.",
+  },
+  {
+    id: 3,
+    rating: 10,
+    text: "Ovo je još jedan u nizu feedbacka koje loadam dinamicki",
+  },
+  {
+    id: 4,
+    rating: 8,
+    text: "Ovo je samo neki lorem ipsum tekst: Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, sequi.",
+  },
+];
+
+export default FeedbackData;
